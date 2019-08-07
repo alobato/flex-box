3a8091da11bc5e561f5cde40a2abda2c5c3fb69a
Arley Lobato <arleylobato@gmail.com>
2019-08-07 23:13:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Flex Box
+
+> Flex and Box react components
+
+## Install
+
+```sh
+yarn add @alobato/flex-box
+```
+
+## Usage
+
+```js
+import { Flex, Box, css }  from '@alobato/flex-box'
+...
+
+<Flex jc='space-between'>
+
+  <Box ai='center' css={css`color: red;`}>Text</Box>
+
+</Flex>
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+
+import styled from 'styled-components/macro'
+import { style, space, layout, typography, color, position, flexbox } from 'styled-system'
+
+export { css } from 'styled-components'
+
+export const Box = styled.div(
+  {boxSizing: 'border-box'},
+  space,
+  layout,
+  typography,
+  color,
+  position,
+  style({prop: 't', cssProperty: 'top', transformValue: n => /^\d+$/.test(n) ? n + 'px': n}),
+  style({prop: 'r', cssProperty: 'right', transformValue: n => /^\d+$/.test(n) ? n + 'px': n}),
+  style({prop: 'b', cssProperty: 'bottom', transformValue: n => /^\d+$/.test(n) ? n + 'px': n}),
+  style({prop: 'l', cssProperty: 'left', transformValue: n => /^\d+$/.test(n) ? n + 'px': n}),
+  style({prop: 'w', cssProperty: 'width', transformValue: n => /^\d+$/.test(n) ? n + 'px': n}),
+  style({prop: 'h', cssProperty: 'height', transformValue: n => /^\d+$/.test(n) ? n + 'px': n}),
+  style({prop: 'ta', cssProperty: 'textAlign'}),
+  style({prop: 'cursor', cssProperty: 'cursor'}),
+  props => props.css
+)
+
+export const Flex = styled(Box)(
+  {display: 'flex'},
+  flexbox,
+  style({prop: 'jc', cssProperty: 'justifyContent'}),
+  style({prop: 'ai', cssProperty: 'alignItems'})
+)
